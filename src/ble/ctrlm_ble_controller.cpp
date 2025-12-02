082ba2e967efcf45f99eefdf6b7220941e3338dc
Gene Gallagher <Eugene_Gallagher@comcast.com>
2025-12-02 18:16:26 +0000

    apply copilot suggestion

--- src/ble/ctrlm_ble_controller.cpp
+++ src/ble/ctrlm_ble_controller.cpp
@@ -539,10 +539,13 @@ void ctrlm_obj_controller_ble_t::setSupportedIrdbs(uint8_t vendor_support_bitmas
 
    ctrlm_irdb_vendor_info_t rcu_vendor_info{};
    rcu_vendor_info.rcu_support_bitmask = vendor_support_bitmask;
-   irdb->set_vendor(rcu_vendor_info);
+   if (!irdb->set_vendor(rcu_vendor_info)) {
+      XLOGD_ERROR("Failed to set IRDB vendor info for controller <%s> with bitmask <0x%X>.", 
+            ieee_address_get().to_string().c_str(), vendor_support_bitmask);
+
+   }
 
    ctrlm_irdb_vendor_info_t vendor_info{};
-
    if (irdb->get_vendor_info(vendor_info)) {
       XLOGD_INFO("Controller <%s> IRDBs supported bitmask = <0x%X>, which %s support the loaded IRDB plugin vendor <%s>", 
             ieee_address_get().to_string().c_str(), vendor_support_bitmask, 

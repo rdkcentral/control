9cf73cec29294836f29297f8add5ff0400147c47
dwolav200 <David_Wolaver@cable.comcast.com>
2026-01-22 14:49:31 -0500

    fix copilot comment

--- src/voice/ctrlm_voice_obj.h
+++ src/voice/ctrlm_voice_obj.h
@@ -744,7 +744,6 @@ public:
     void                 set_audio_mode(ctrlm_voice_audio_settings_t *settings);
     void                 audio_state_set(bool session);
     bool                 vsdk_is_privacy_enabled(void);
-    double               vsdk_keyword_sensitivity_limit_check(double sensitivity);
     void                 pre_session_terminate(std::function<void(ctrlm_voice_start_audio_params_t *)> cb_start_audio,
                                                ctrlm_voice_start_audio_params_t *cb_audio_start_params,
                                                ctrlm_voice_session_rsp_confirm_t *cb_confirm,

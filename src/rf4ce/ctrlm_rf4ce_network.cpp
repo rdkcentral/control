f82910f1d9ba6873824b26a76730cdc9aafc45c3
Kelvin Lu <119349872+klu339@users.noreply.github.com>
2026-04-20 11:55:59 -0400

    RDKEMW-17330,RDKEMW-17315: ctrlm hotfix release 1.1.7.1 (support/8.4.4.0) (#204)
    * RDKEMW-17187 : PTT audio buffering feature aborting voice session before mic key release
    
    * RDKEMW-16763 : rf4ce init failure causes deadlock
    
    * RDKEMW-17330,RDKEMW-17315: CHANGELOG update for ctrlm hotfix release 1.1.7.1
    
    ---------
    
    Co-authored-by: dwolav200 <David_Wolaver@cable.comcast.com>

--- src/rf4ce/ctrlm_rf4ce_network.cpp
+++ src/rf4ce/ctrlm_rf4ce_network.cpp
@@ -35,6 +35,7 @@
 #include <algorithm>
 #include <regex>
 #include <limits.h>
+#include <errno.h>
 #include <sys/sysinfo.h>
 #include "ctrlm.h"
 #include "ctrlm_log.h"
@@ -411,8 +412,31 @@ ctrlm_hal_result_t ctrlm_obj_network_rf4ce_t::hal_init_request(GThread *ctrlm_ma
 
    // Block until initialization is complete or a timeout occurs
    XLOGD_INFO("Waiting for %s initialization...", name_get());
-   sem_wait(&semaphore_);
-   sem_destroy(&semaphore_);
+   struct timespec timeout;
+   clock_gettime(CLOCK_REALTIME, &timeout);
+   timeout.tv_sec += 2;  // this operation should complete in under 100 ms under normal circumstances
+   
+   int sem_result = -1;
+   do {
+      errno = 0;
+      sem_result = sem_timedwait(&semaphore_, &timeout);
+      if(sem_result == -1 && errno == EINTR) {
+         XLOGD_INFO("interrupted");
+      } else {
+         break;
+      }
+   } while(1);
+
+   if(sem_result == -1) {
+      if(errno == ETIMEDOUT) {
+         XLOGD_ERROR("Timeout waiting for %s initialization", name_get());
+      } else {
+         XLOGD_ERROR("Error waiting for %s initialization: %s", name_get(), strerror(errno));
+      }
+      init_result_ = CTRLM_HAL_RESULT_ERROR;
+   } else {
+      sem_destroy(&semaphore_);
+   }
 
    ready_ = (CTRLM_HAL_RESULT_SUCCESS == init_result_);
 

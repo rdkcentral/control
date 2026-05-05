e977b9f12a0e0d05021ea72aab21ef082a9fe7e7
dwolav200 <David_Wolaver@cable.comcast.com>
2026-05-05 10:51:41 -0400

    add newline

--- src/voice/endpoints/ctrlm_voice_endpoint_ws_nextgen.h
+++ src/voice/endpoints/ctrlm_voice_endpoint_ws_nextgen.h
@@ -68,6 +68,7 @@ protected:
     static void ctrlm_voice_handler_ws_nextgen_tv_mute(bool mute, void *user_data);
     static void ctrlm_voice_handler_ws_nextgen_tv_power(bool power, bool toggle, void *user_data);
     static void ctrlm_voice_handler_ws_nextgen_tv_volume(bool up, uint32_t repeat_count, void *user_data);
+
 protected:
     void       *xrsv_obj_ws_nextgen;
 

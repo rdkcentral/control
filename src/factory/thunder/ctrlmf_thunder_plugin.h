92fa0cc60e23d6570156c94239915fb77f28da58
balav08 <54432605+balav08@users.noreply.github.com>
2026-07-02 11:20:13 +0530

    Update ctrlmf_thunder_plugin.h

--- src/factory/thunder/ctrlmf_thunder_plugin.h
+++ src/factory/thunder/ctrlmf_thunder_plugin.h
@@ -107,6 +107,16 @@ protected:
     bool call_plugin(std::string method, void *params, void *response);
 
     /**
+     * This functions is used to call a Thunder Plugin method with retry support.
+     * @param method The method in which the user wants to call.
+     * @param params The WPEFramework JsonObject containing the parameters for the call.
+     * @param response The WPEFramework JsonObject containing the response from the call.
+     * @param retries The number of retries if the call times out.
+     * @return True if the call succeeded, otherwise False.
+     */
+    bool call_plugin(std::string method, void *params, void *response, unsigned int retries);
+
+    /**
      * This functions is used to call a Thunder Controller method.
      * @param method The method in which the user wants to call.
      * @param params The WPEFramework JsonObject containing the parameters for the call. (We can't include WPEFramework headers in controlMgr .h files as their logging macros clash)

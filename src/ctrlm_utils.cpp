4acc01cad3a4d350caa9c4e4d60839e8f9d516ae
balav08 <54432605+balav08@users.noreply.github.com>
2026-07-03 18:02:31 +0530

    Update ctrlm_utils.cpp

--- src/ctrlm_utils.cpp
+++ src/ctrlm_utils.cpp
@@ -35,10 +35,24 @@
 #include <linux/input.h>
 #include <uuid/uuid.h>
 
+#ifdef CTRLM_USE_THUNDER_FR_DS
 #include <curl/curl.h>
-#include <regex>
 #include "thunder/plugins/ctrlm_thunder_plugin_display_settings.h"
 #include "thunder/plugins/ctrlm_thunder_plugin_front_panel.h"
+#else
+#include "host.hpp"
+#include "exception.hpp"
+#include "videoOutputPort.hpp"
+#include "videoOutputPortType.hpp"
+#include "videoOutputPortConfig.hpp"
+#include "audioOutputPort.hpp"
+#include "frontPanelIndicator.hpp"
+#include "manager.hpp"
+#include "dsMgr.h"
+#include "dsRpc.h"
+#include "dsDisplay.h"
+#endif
+#include <regex>
 
 using std::get;
 using std::map;
@@ -1534,16 +1548,46 @@ char *ctrlm_do_regex(char *re, char *str) {
 }
 
 bool ctrlm_dsmgr_init() {
+#ifdef CTRLM_USE_THUNDER_FR_DS
    // DSMgr initialization is no longer needed; Thunder plugins self-initialise.
    return true;
+#else
+   if(device::Manager::IsInitialized) {
+      XLOGD_INFO("DSMgr already initialized");
+      return true;
+   }
+   try {
+      device::Manager::Initialize();
+      XLOGD_INFO("DSMgr is initialized");
+   }
+   catch (...) {
+      XLOGD_WARN("Failed to initialize DSMgr");
+      return false;
+   }
+   return true;
+#endif
 }
 
 bool ctrlm_dsmgr_deinit() {
+#ifdef CTRLM_USE_THUNDER_FR_DS
    // DSMgr deinitialization is no longer needed; Thunder plugins self-manage.
    return true;
+#else
+   try {
+      if(device::Manager::IsInitialized) {
+         device::Manager::DeInitialize();
+      }
+   }
+   catch(...) {
+      XLOGD_WARN("Failed to deinitialize DSMgr");
+      return false;
+   }
+   return true;
+#endif
 }
 
 bool ctrlm_dsmgr_mute_audio(bool mute) {
+#ifdef CTRLM_USE_THUNDER_FR_DS
    dsAudioDuckingAction_t action = mute ? dsAUDIO_DUCKINGACTION_START : dsAUDIO_DUCKINGACTION_STOP;
    auto *ds = Thunder::DisplaySettings::ctrlm_thunder_plugin_display_settings_t::getInstance();
    if(!ds) {
@@ -1557,6 +1601,18 @@ bool ctrlm_dsmgr_mute_audio(bool mute) {
       XLOGD_WARN("Muting sound via Thunder failed");
    }
    return ret;
+#else
+   try {
+      dsAudioDuckingAction_t action = mute ? dsAUDIO_DUCKINGACTION_START : dsAUDIO_DUCKINGACTION_STOP;
+      device::Host::getInstance().getAudioOutputPort("SPEAKER0").setAudioDucking(action, dsAUDIO_DUCKINGTYPE_ABSOLUTE, mute ? 0 : 100);
+      XLOGD_INFO("Audio is %smuted", mute?"":"un-");
+   }
+   catch(std::exception& error) {
+      XLOGD_WARN("Muting sound error : %s", error.what());
+      return false;
+   }
+   return true;
+#endif
 }
 
 bool ctrlm_dsmgr_duck_audio(bool enable, bool relative, double vol) {
@@ -1564,6 +1620,7 @@ bool ctrlm_dsmgr_duck_audio(bool enable, bool relative, double vol) {
       XLOGD_ERROR("Invalid volume");
       return false;
    }
+#ifdef CTRLM_USE_THUNDER_FR_DS
    unsigned char level = (unsigned char)((vol * 100) + 0.5);
    dsAudioDuckingAction_t action = enable   ? dsAUDIO_DUCKINGACTION_START  : dsAUDIO_DUCKINGACTION_STOP;
    dsAudioDuckingType_t   type   = relative ? dsAUDIO_DUCKINGTYPE_RELATIVE : dsAUDIO_DUCKINGTYPE_ABSOLUTE;
@@ -1584,9 +1641,31 @@ bool ctrlm_dsmgr_duck_audio(bool enable, bool relative, double vol) {
       XLOGD_WARN("Ducking sound via Thunder failed");
    }
    return ret;
+#else
+   try {
+      unsigned char level = (unsigned char)((vol * 100) + 0.5);
+
+      dsAudioDuckingAction_t action = enable   ? dsAUDIO_DUCKINGACTION_START  : dsAUDIO_DUCKINGACTION_STOP;
+      dsAudioDuckingType_t   type   = relative ? dsAUDIO_DUCKINGTYPE_RELATIVE : dsAUDIO_DUCKINGTYPE_ABSOLUTE;
+
+      device::Host::getInstance().getAudioOutputPort("SPEAKER0").setAudioDucking(action, type, level);
+
+      if(enable) {
+         XLOGD_INFO("Audio ducking enabled - type <%s> level <%u%%>", relative ? "RELATIVE" : "ABSOLUTE", level);
+      } else {
+         XLOGD_INFO("Audio ducking disabled");
+      }
+   }
+   catch(std::exception& error) {
+      XLOGD_WARN("Ducking sound error : %s", error.what());
+      return false;
+   }
+   return true;
+#endif
 }
 
 bool ctrlm_dsmgr_LED(bool on) {
+#ifdef CTRLM_USE_THUNDER_FR_DS
    auto *fp = Thunder::FrontPanel::ctrlm_thunder_plugin_front_panel_t::getInstance();
    if(!fp) {
       XLOGD_ERROR("FrontPanel plugin not available");
@@ -1597,6 +1676,21 @@ bool ctrlm_dsmgr_LED(bool on) {
       XLOGD_WARN("LED control via Thunder failed");
    }
    return ret;
+#else
+   try {
+      device::FrontPanelIndicator &led =  device::FrontPanelIndicator::getInstance("Power");
+      if (on) {
+         led.setColor(0xFFFFFF);
+         led.setBrightness(100);
+      }
+      led.setState(on);
+   }
+   catch(std::exception& error) {
+      XLOGD_WARN("LED error : %s", error.what());
+      return false;
+   }
+   return true;
+#endif
 }
 
 bool ctrlm_is_voice_assistant(ctrlm_rcu_controller_type_t controller_type) {
